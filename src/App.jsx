ab5a62d74abe0d864426332bc5b03baeb9c24db1
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2024-10-12 23:33:04 +0330

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+// import { createElement } from "react";
+import { TodoList } from "./TodoList";
+import { TodoListClass } from "./TodoListClass";
+import { Name } from "./Name";
+
 function App() {
-  return "HELLO WORLD";
+  // return createElement(
+  //   "h3",
+  //   { id: "5" },
+  //   "hello world",
+  //   createElement("span", null, "Bye")
+  // );
+  // return (
+  //   <h1 id="5">
+  //     Hello World
+  //     <span>Bye</span>
+  //   </h1>
+  // );
+
+  // return createElement("div", { id: "largeDiv", className: "large" }, "hi");
+  // return (
+  //   <div className="large" id="largeDiv">
+  //     <label htmlFor="inputId">
+  //       test
+  //     </label>
+  //     <input id="inputId" type="number" defaultValue={3}></input>
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList />
+      <TodoListClass />
+      <Name />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/TodoListClass.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class TodoListClass extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Item Class 1</li>
+        <li>Item Class 2</li>
+        <li>Item Class 3</li>
+        <li>Item Class 4</li>
+      </ul>
+    );
+  }
+}
